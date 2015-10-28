4dd18c1ab0ec306d37f39fe526d43fa0312bc780
Nathan Perkins <lnp@bu.edu>
2015-10-28 14:19:16 -0400

    Initial Commit

---
+++ SyllableDetector/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SyllableDetector
+//
+//  Created by Nathan Perkins on 10/28/15.
+//  Copyright © 2015 Gardner Lab. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SyllableDetector/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SyllableDetector
+//
+//  Created by Nathan Perkins on 10/28/15.
+//  Copyright © 2015 Gardner Lab. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
