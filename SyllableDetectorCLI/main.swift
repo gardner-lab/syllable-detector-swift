462c1e10701c0dd1449fabed98f208baa432eb1e
Nathan Perkins <nathan@maxmo.net>
2016-06-02 16:03:37 -0400

    Rearrange code and add new target

---
+++ SyllableDetectorCLI/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SyllableDetectorCLI
+//
+//  Created by Nathan Perkins on 6/2/16.
+//  Copyright © 2016 Gardner Lab. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
