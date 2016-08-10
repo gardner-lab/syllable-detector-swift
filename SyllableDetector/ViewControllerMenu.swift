5093ba1fbf12a26c4e86be1d71580cef378465e3
Nathan Perkins <nathan@maxmo.net>
2016-08-10 14:29:24 -0400

    Add Arduino devices to menu

--- SyllableDetector/ViewControllerMenu.swift
+++ SyllableDetector/ViewControllerMenu.swift
@@ -8,6 +8,7 @@
 
 import Cocoa
 import AudioToolbox
+import ORSSerial
 
 class ViewControllerMenu: NSViewController, WindowControllerProcessorDelegate {
     @IBOutlet weak var selectInput: NSPopUpButton!
@@ -17,12 +18,25 @@ class ViewControllerMenu: NSViewController, WindowControllerProcessorDelegate {
     var openProcessors = [NSWindowController]()
     var openSimulators = [NSWindowController]()
     
+    @objc class DeviceRepresentation: NSObject {
+        var audioDeviceID: AudioDeviceID?
+        var arduinoDevicePath: String?
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // reload devices
         buttonLaunch.isEnabled = false
-        reloadDevices()
+    }
+    
+    override func viewDidAppear() {
+        super.viewDidAppear()
+        
+        // listen for serial changes
+        let nc = NotificationCenter.default
+        nc.addObserver(self, selector: #selector(ViewControllerMenu.serialPortsWereConnected(_:)), name: NSNotification.Name.ORSSerialPortsWereConnected, object: nil)
+        nc.addObserver(self, selector: #selector(ViewControllerMenu.serialPortsWereDisconnected(_:)), name: NSNotification.Name.ORSSerialPortsWereDisconnected, object: nil)
         
         // listen
         do {
@@ -34,15 +48,38 @@ class ViewControllerMenu: NSViewController, WindowControllerProcessorDelegate {
         catch {
             DLog("Unable to add device change listener: \(error)")
         }
+        
+        // reload devices
+        reloadDevices()
     }
     
     override func viewDidDisappear() {
+        // remove notification center
+        NotificationCenter.default.removeObserver(self)
+        
         // remove listener
         AudioInterface.destroyListenerForDeviceChange(withIdentifier: self)
         
         // terminate
         if 0 == openProcessors.count && 0 == openSimulators.count {
             NSApp.terminate(nil)
+        }
+    }
+    
+    // serial port
+    func serialPortsWereConnected(_ notification: Notification) {
+        if let userInfo = (notification as NSNotification).userInfo {
+            let connectedPorts = userInfo[ORSConnectedSerialPortsKey] as! [ORSSerialPort]
+            DLog("Ports were connected: \(connectedPorts)")
+            reloadDevices()
+        }
+    }
+    
+    func serialPortsWereDisconnected(_ notification: Notification) {
+        if let userInfo = (notification as NSNotification).userInfo {
+            let disconnectedPorts: [ORSSerialPort] = userInfo[ORSDisconnectedSerialPortsKey] as! [ORSSerialPort]
+            DLog("Ports were disconnected: \(disconnectedPorts)")
+            reloadDevices()
         }
     }
     
@@ -58,21 +95,26 @@ class ViewControllerMenu: NSViewController, WindowControllerProcessorDelegate {
         }
         
         // get input
-        let selectedInput = selectInput.selectedTag()
-        let selectedOutput = selectOutput.selectedTag()
+//        let selectedInput = selectInput.selectedItem?.representedObject
+//        let selectedOutput = selectOutput.selectedItem?.representedObject
         
         // rebuild inputs
         selectInput.removeAllItems()
         selectInput.addItem(withTitle: "Input")
         for d in devices {
             if 0 < d.streamsInput {
+                // representation
+                let obj = DeviceRepresentation()
+                obj.audioDeviceID = d.deviceID
+                
+                // menu item
                 let item = NSMenuItem()
                 item.title = d.deviceName
-                item.tag = Int(d.deviceID)
+                item.representedObject = obj
                 selectInput.menu?.addItem(item)
             }
         }
-        selectInput.selectItem(withTag: selectedInput)
+//        selectInput.selectItem(withTag: selectedInput)
         selectInput.synchronizeTitleAndSelectedItem()
         
         // rebuild outputs
@@ -80,18 +122,34 @@ class ViewControllerMenu: NSViewController, WindowControllerProcessorDelegate {
         selectOutput.addItem(withTitle: "Output")
         for d in devices {
             if 0 < d.streamsOutput {
+                // representation
+                let obj = DeviceRepresentation()
+                obj.audioDeviceID = d.deviceID
+                
+                // menu item
                 let item = NSMenuItem()
                 item.title = d.deviceName
-                item.tag = Int(d.deviceID)
+                item.representedObject = obj
                 selectOutput.menu?.addItem(item)
             }
         }
-        selectOutput.selectItem(withTag: selectedOutput)
+        for port in ORSSerialPortManager.shared().availablePorts {
+            // representation
+            let obj = DeviceRepresentation()
+            obj.arduinoDevicePath = port.path
+            
+            // menu item
+            let item = NSMenuItem()
+            item.title = "Arduino (\(port.name))"
+            item.representedObject = obj
+            selectOutput.menu?.addItem(item)
+        }
+//        selectOutput.selectItem(withTag: selectedOutput)
         selectOutput.synchronizeTitleAndSelectedItem()
     }
     
     @IBAction func selectDevice(_ sender: NSPopUpButton) {
-        buttonLaunch.isEnabled = (0 < selectInput.selectedTag() && 0 < selectOutput.selectedTag())
+        buttonLaunch.isEnabled = (nil != selectInput.selectedItem?.representedObject && nil != selectOutput.selectedItem?.representedObject)
     }
     
     @IBAction func buttonSimulate(_ sender: NSButton) {
@@ -105,19 +163,47 @@ class ViewControllerMenu: NSViewController, WindowControllerProcessorDelegate {
     @IBAction func buttonLaunch(_ sender: NSButton) {
         guard let sb = storyboard, let controller = sb.instantiateController(withIdentifier: "Processor") as? WindowControllerProcessor else { return }
         
+        // get input device representation
+        guard let menuItemInput = selectInput.selectedItem, let objInput = menuItemInput.representedObject, let deviceRepresentationInput = objInput as? DeviceRepresentation else {
+            return
+        }
+        
+        // get output device representation
+        guard let menuItemOutput = selectOutput.selectedItem, let objOutput = menuItemOutput.representedObject, let deviceRepresentationOutput = objOutput as? DeviceRepresentation else {
+            return
+        }
+        
         // get input device
-        guard let deviceInput = AudioInterface.AudioDevice(deviceID: AudioDeviceID(selectInput.selectedTag())) else {
+        guard let inputAudioID = deviceRepresentationInput.audioDeviceID, let deviceInput = AudioInterface.AudioDevice(deviceID: inputAudioID) else {
             DLog("input device no longer valid")
             reloadDevices()
             return
         }
         
         // get output device
-        guard let deviceOutput = AudioInterface.AudioDevice(deviceID: AudioDeviceID(selectOutput.selectedTag())) else {
-            DLog("output device no longer valid")
-            reloadDevices()
+        let deviceOutput: ViewControllerProcessor.OutputDevice
+        if let outputAudioID = deviceRepresentationOutput.audioDeviceID {
+            guard let o = AudioInterface.AudioDevice(deviceID: outputAudioID) else {
+                DLog("output device no longer valid")
+                reloadDevices()
+                return
+            }
+            deviceOutput = .audio(interface: o)
+        }
+        else if let outputArduinoPath = deviceRepresentationOutput.arduinoDevicePath {
+            guard let o = ORSSerialPort(path: outputArduinoPath) else {
+                DLog("output device no longer valid")
+                reloadDevices()
+                return
+            }
+            deviceOutput = .arduino(port: o)
+        }
+        else {
+            DLog("no output device found")
             return
         }
+        
+        DLog("\(deviceOutput)")
         
         // setup controller
         if let vc = controller.contentViewController, let vcp = vc as? ViewControllerProcessor {
